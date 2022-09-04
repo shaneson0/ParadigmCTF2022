2c272eb7f90f3ece30eb29ef746bcb999b7ef09e
oker <oker@okerdeMacBook-Pro-98.local>
2022-09-04 16:43:29 +0800

    CTF 2022

---
+++ scripts/deploy-rescue.js
@@ -0,0 +1,21 @@
+
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+async function main() {
+  // We get the contract to deploy
+  const SetupDeploy = await hre.ethers.getContractFactory("Setup");
+  const Setup = await SetupDeploy.deploy({value: ethers.utils.parseEther("10")});
+  await Setup.deployed();
+
+  console.log("Setup deployed to:", Setup.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
